0cdb4f3fa4c637d47779b8492c9fa013d500f1da
Em Zhan <zqianem@gmail.com>
2019-09-30 11:49:33 -0400

    Make script more portable
    Based on recommendations from ShellCheck

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ "$1" == "" ]
+if [ "$1" = "" ]
 then
   echo Tests not run, missing first argument
   echo usage: $0 [path_to_interpreter_script]
@@ -18,7 +18,7 @@ then
   exit 1
 fi
 
-tests=**/*.code
+tests="**/*.code"
 
 for t in $tests
 do
@@ -32,26 +32,26 @@ do
     data=empty.data
   fi
 
-  echo -n "Testing $t... "
+  printf "Testing %s... " "$t"
 
   if [ $t != "${t%bad*}" ]
   then
     output=$($1 $code $data)
     if [ "$output" != "${output%ERROR:*}" ]
     then
-      echo -e "\033[0;32mPASSED\033[0m"
+      printf "\033[0;32mPASSED\033[0m\n"
     else
-      echo -e "\033[0;31mFAILED\033[0m"
-      echo -e "\033[1mexpected error, got following output:\033[0m"
+      printf "\033[0;31mFAILED\033[0m\n"
+      printf "\033[1mexpected error, got following output:\033[0m\n"
       echo "$output"
     fi
   else
     output=$($1 $code $data | diff -u --color=always - $expected)
     if [ $? -eq 0 ]
     then
-      echo -e "\033[0;32mPASSED\033[0m"
+      printf "\033[0;32mPASSED\033[0m\n"
     else
-      echo -e "\033[0;31mFAILED\033[0m"
+      printf "\033[0;31mFAILED\033[0m\n"
       echo "$output"
     fi
   fi
